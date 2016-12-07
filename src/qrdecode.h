e0cc084bdfe79653631104be17f534ed24f96ceb
Greg Zetko <gergo@newjanson.com>
2016-12-06 21:50:59 -0500

    Moved zxing includes to qrdecode.h

--- src/qrdecode.h
+++ src/qrdecode.h
@@ -12,4 +12,17 @@
 namespace qrdecode{
 	std::string decodeQR(cv::Mat qr_gray);
 }
+#include <zxing/common/Counted.h>
+#include <zxing/Binarizer.h>
+#include <zxing/MultiFormatReader.h>
+#include <zxing/Result.h>
+#include <zxing/ReaderException.h>
+#include <zxing/common/GlobalHistogramBinarizer.h>
+#include <zxing/Exception.h>
+#include <zxing/common/IllegalArgumentException.h>
+#include <zxing/BinaryBitmap.h>
+#include <zxing/DecodeHints.h>
+#include <zxing/qrcode/QRCodeReader.h>
+#include <zxing/MultiFormatReader.h>
+#include "zxing/MatSource.h"
 #endif /* SRC_QRDECODE_H_ */
--- src/qrscan.h
+++ src/qrscan.h
@@ -9,19 +9,6 @@
 #include <opencv2/text.hpp>
 #include <iostream>
 #include <sstream>
-#include <zxing/common/Counted.h>
-#include <zxing/Binarizer.h>
-#include <zxing/MultiFormatReader.h>
-#include <zxing/Result.h>
-#include <zxing/ReaderException.h>
-#include <zxing/common/GlobalHistogramBinarizer.h>
-#include <zxing/Exception.h>
-#include <zxing/common/IllegalArgumentException.h>
-#include <zxing/BinaryBitmap.h>
-#include <zxing/DecodeHints.h>
-#include <zxing/qrcode/QRCodeReader.h>
-#include <zxing/MultiFormatReader.h>
-#include "zxing/MatSource.h"
 #include "qrdecode.h"
 int testproc();
 
